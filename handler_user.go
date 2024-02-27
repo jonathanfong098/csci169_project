4b4257ec1e616d37540e8eb970a011f6c9729ef9
jonathanfong098 <jonathanfong098@gmail.com>
2024-02-26 17:46:01 -0800

    add api keys for authentication

--- handler_user.go
+++ handler_user.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/jonathanfong098/csci169project/internal/auth"
 	"github.com/jonathanfong098/csci169project/internal/database"
 )
 
@@ -29,6 +30,22 @@ func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request)
 	})
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't create user")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, databaseUserToUser(user))
+}
+
+func (cfg *apiConfig) handlerUsersGet(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find api key")
+		return
+	}
+
+	user, err := cfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't get user")
 		return
 	}
 
